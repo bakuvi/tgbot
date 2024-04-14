0b7df4a96608536e5adf6a55a5404f6a325f300d
bakuvi <emin.feyziyev@gmail.com>
2024-04-13 22:04:20 -0400

    added base thing to launch bot

---
+++ src/main/java/com/example/tgbot/service/TelegramBot.java
@@ -0,0 +1,64 @@
+package com.example.tgbot.service;
+
+import com.example.tgbot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig botConfig;
+
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+
+                    startCommandRecieved(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, command not recognized");
+            }
+        }
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getBotToken();
+    }
+
+    private void startCommandRecieved(long chatId, String firstName) {
+        String answer = "Hi, " + firstName + ", nice to meet u";
+        sendMessage(chatId, answer);
+
+    }
+
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
